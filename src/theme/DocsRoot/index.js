65a1d1c5352ca2c0fabd77936c44dee290be0fbf
Your Name <you@example.com>
2024-03-04 20:52:24 +0800

    first commit

---
+++ src/theme/DocsRoot/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import DocsRoot from '@theme-original/DocsRoot';
+
+export default function DocsRootWrapper(props) {
+  return (
+    <>
+      <DocsRoot {...props} />
+    </>
+  );
+}
