6dcf9a542a6babb674137e376282ef7f146b87f7
Your Name <you@example.com>
2024-09-12 16:56:45 +0800

    test

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,3 @@
-rm -rf /www/wwwroot/blog.chdl.fun/
-mv build/* /www/wwwroot/blog.chdl.fun/
+rm -rf /www/wwwroot/blog.chdl.fun/ChBlog/
+mkdir -p /www/wwwroot/blog.chdl.fun/ChBlog/
+mv build/* /www/wwwroot/blog.chdl.fun/ChBlog/
