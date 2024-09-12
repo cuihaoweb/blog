3b33a2d05283e5a781b7161c68f3c67b51d05171
cuihaoweb <58589917+cuihaoweb@users.noreply.github.com>
2024-09-12 09:21:05 +0000

    Deploying to rb from @ cuihaoweb/blog@72afc30baa825b8c17e91f84afb778744bcb0db8 🚀

--- script/build.sh
+++ script/build.sh
@@ -1,3 +1,4 @@
-rm -rf /www/wwwroot/blog.chdl.fun/ChBlog/
+rm -rf /www/wwwroot/blog.chdl.fun/
 mkdir -p /www/wwwroot/blog.chdl.fun/ChBlog/
+mv build/{robots.txt,sitemap.xml} /www/wwwroot/blog.chdl.fun/
 mv build/* /www/wwwroot/blog.chdl.fun/ChBlog/
